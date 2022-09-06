d9a22e911409ad29921e0d1b66e3fba072a6dfd3
Dmitriy Vetutnev <d.vetutnev@gmail.com>
2022-09-06 19:15:59 +0300

    Add SequenceBarrier ctor/dtor

---
+++ include/sequence_barrier.h
@@ -0,0 +1,32 @@
+#pragma once
+
+#include "event.h"
+#include "sequence_traits.h"
+
+template<typename TSequence = std::size_t,
+         typename Traits = SequenceTraits<TSequence>>
+class SequenceBarrier
+{
+public:
+    SequenceBarrier(TSequence initialSequence = Traits::initial_sequence);
+    ~SequenceBarrier();
+
+private:
+    class Awaiter;
+
+    std::atomic<TSequence> _lastKnowPublished;
+    std::atomic<Awaiter*> _awaiters;
+};
+
+template<typename TSequence, typename Traits>
+SequenceBarrier<TSequence, Traits>::SequenceBarrier(TSequence initialSequence)
+    :
+    _lastKnowPublished{initialSequence},
+    _awaiters{nullptr}
+{}
+
+template<typename TSequence, typename Traits>
+SequenceBarrier<TSequence, Traits>::~SequenceBarrier()
+{
+    assert(_awaiters.load(std::memory_order_relaxed) == nullptr);
+}
