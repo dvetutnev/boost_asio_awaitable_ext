290409864d0fdc1d03112871c2c745231fe55e43
Dmitriy Vetutnev <d.vetutnev@gmail.com>
2022-10-11 02:26:08 +0300

    Add test multiply awailters

--- test/test_sequence_barrier.cpp
+++ test/test_sequence_barrier.cpp
@@ -40,7 +40,7 @@ BOOST_AUTO_TEST_CASE_TEMPLATE(previous, T, SequenceTypes)
     ioContext.run();
 }
 
-BOOST_AUTO_TEST_CASE(wait_until_publish)
+BOOST_AUTO_TEST_CASE(single_consumer)
 {
     SequenceBarrier<> barrier;
     bool reachedA = false;
@@ -97,6 +97,94 @@ BOOST_AUTO_TEST_CASE(wait_until_publish)
     auto main = [&]() -> awaitable<void> {
         using namespace experimental::awaitable_operators;
         co_await(consumer() && producer());
+        co_return;
+    };
+
+    io_context ioContext;
+    co_spawn(ioContext, main(), detached);
+    ioContext.run();
+}
+
+BOOST_AUTO_TEST_CASE(multiply_consumers)
+{
+    SequenceBarrier<> barrier;
+    bool reachedA = false;
+    bool reachedB = false;
+    bool reachedC = false;
+    bool reachedD = false;
+    bool reachedE = false;
+    bool reachedF = false;
+    bool reachedG = false;
+    bool reachedH = false;
+
+    auto consumer10 = [&]() -> awaitable<void> {
+        BOOST_TEST(co_await barrier.wait_until_published(0) == 0);
+        reachedA = true;
+        BOOST_TEST(co_await barrier.wait_until_published(10) == 10);
+        reachedB = true;
+    };
+
+    auto consumer17 = [&]() -> awaitable<void> {
+        reachedC = true;
+        BOOST_TEST(co_await barrier.wait_until_published(17) == 18);
+        reachedD = true;
+    };
+
+    auto consumer18 = [&]() -> awaitable<void> {
+        reachedE = true;
+        BOOST_TEST(co_await barrier.wait_until_published(18) == 18);
+        reachedF = true;
+    };
+
+    auto consumer20 = [&]() -> awaitable<void> {
+        reachedG = true;
+        BOOST_TEST(co_await barrier.wait_until_published(20) == 20);
+        reachedH = true;
+    };
+
+    auto producer = [&]() -> awaitable<void> {
+        BOOST_TEST(!reachedA);
+        BOOST_TEST(!reachedB);
+        BOOST_TEST(reachedC);
+        BOOST_TEST(!reachedD);
+        BOOST_TEST(reachedE);
+        BOOST_TEST(!reachedF);
+        BOOST_TEST(reachedG);
+        BOOST_TEST(!reachedH);
+
+        barrier.publish(0);
+        co_await schedule(co_await this_coro::executor);
+        BOOST_TEST(reachedA);
+        BOOST_TEST(!reachedB);
+        BOOST_TEST(!reachedD);
+        BOOST_TEST(!reachedF);
+        BOOST_TEST(!reachedH);
+
+        barrier.publish(10);
+        co_await schedule(co_await this_coro::executor);
+        BOOST_TEST(reachedB);
+        BOOST_TEST(!reachedD);
+        BOOST_TEST(!reachedF);
+        BOOST_TEST(!reachedH);
+
+        barrier.publish(18);
+        co_await schedule(co_await this_coro::executor);
+        BOOST_TEST(reachedD);
+        BOOST_TEST(reachedF);
+        BOOST_TEST(!reachedH);
+
+        barrier.publish(20);
+        co_await schedule(co_await this_coro::executor);
+        BOOST_TEST(reachedH);
+    };
+
+    auto main = [&]() -> awaitable<void> {
+        using namespace experimental::awaitable_operators;
+        co_await(consumer10() &&
+                 consumer17() &&
+                 consumer18() &&
+                 consumer20() &&
+                 producer());
         co_return;
     };
 
