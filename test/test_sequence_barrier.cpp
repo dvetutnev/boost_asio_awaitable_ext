f4a0ba56f85f517de7a89e60889a6535edb344d2
Dmitriy Vetutnev <d.vetutnev@gmail.com>
2023-04-17 22:11:03 +0300

    Move TSAN test SequenceBarrier

--- test/test_sequence_barrier.cpp
+++ test/test_sequence_barrier.cpp
@@ -1,6 +1,5 @@
 #include "sequence_barrier.h"
 #include "schedule.h"
-#include "sequence_barrier_mock_awaiter.h"
 
 #include <boost/asio/io_context.hpp>
 #include <boost/asio/detached.hpp>
@@ -269,66 +268,6 @@ BOOST_AUTO_TEST_CASE(multithread)
         static_cast<std::uint64_t>(iterationCount) * static_cast<std::uint64_t>(1 + iterationCount) / 2;
 
     BOOST_TEST(result == expectedResult);
-}
-
-using SequenceTypesTSan = boost::mpl::list<std::uint8_t,
-                                           std::uint16_t>;
-
-BOOST_TEST_DECORATOR(* unit_test::disabled())
-BOOST_AUTO_TEST_CASE_TEMPLATE(tsan, T, SequenceTypesTSan)
-{
-    using BaseBarrier = SequenceBarrier<T, SequenceTraits<T>, MockAwaiter<T>>;
-    struct Barrier : BaseBarrier
-    {
-        using BaseBarrier::add_awaiter;
-    };
-    Barrier barrier;
-
-    std::atomic_bool producerDone = false;
-    MockAwaitersStorage<T> awaiters;
-
-    auto consumer = [&, previos = T{}] mutable
-    {
-        for (;;)
-        {
-            if (producerDone.load(std::memory_order_acquire)) {
-                break;
-            }
-            const T lastPublished = barrier.last_published();
-            if (lastPublished == previos) {
-                continue;
-            }
-            previos = lastPublished;
-
-            constexpr T quater = std::numeric_limits<T>::max() / 4;
-
-            if (auto* awaiter = awaiters.get_upper(lastPublished + quater); awaiter != nullptr) {
-                barrier.add_awaiter(awaiter);
-            }
-            if (auto* awaiter = awaiters.get_lower(lastPublished - quater); awaiter != nullptr) {
-                barrier.add_awaiter(awaiter);
-            }
-        }
-    };
-
-    auto producer = [&]()
-    {
-        for (std::size_t i = 0; i <= std::numeric_limits<T>::max(); i++) {
-            barrier.publish(i);
-        }
-        producerDone.store(true, std::memory_order_release);
-        // Resume remaining awaiters
-        for (std::size_t i = 0; i <= std::numeric_limits<T>::max(); i++) {
-            barrier.publish(i);
-        }
-    };
-
-    thread_pool tp{2};
-    any_io_executor executorA = tp.get_executor();
-    any_io_executor executorB = tp.get_executor();
-    post(executorA, consumer);
-    post(executorB, producer);
-    tp.join();
 }
 
 BOOST_AUTO_TEST_SUITE_END();
---
+++ test/tsan/sequence_barrier.cpp
@@ -0,0 +1,70 @@
+#include "sequence_barrier.h"
+#include "sequence_barrier_mock_awaiter.h"
+
+#include <boost/asio/thread_pool.hpp>
+
+#include <boost/test/unit_test.hpp>
+#include <boost/mpl/list.hpp>
+
+namespace boost::asio::awaitable_ext::test {
+
+using SequenceTypes = boost::mpl::list<std::uint8_t,
+                                       std::uint16_t>;
+
+BOOST_AUTO_TEST_CASE_TEMPLATE(_SequenceBarrier, T, SequenceTypes)
+{
+    using BaseBarrier = SequenceBarrier<T, SequenceTraits<T>, MockAwaiter<T>>;
+    struct Barrier : BaseBarrier
+    {
+        using BaseBarrier::add_awaiter;
+    };
+    Barrier barrier;
+
+    std::atomic_bool producerDone = false;
+    MockAwaitersStorage<T> awaiters;
+
+    auto consumer = [&, previos = T{}] mutable
+    {
+        for (;;)
+        {
+            if (producerDone.load(std::memory_order_acquire)) {
+                break;
+            }
+            const T lastPublished = barrier.last_published();
+            if (lastPublished == previos) {
+                continue;
+            }
+            previos = lastPublished;
+
+            constexpr T quater = std::numeric_limits<T>::max() / 4;
+
+            if (auto* awaiter = awaiters.get_upper(lastPublished + quater); awaiter != nullptr) {
+                barrier.add_awaiter(awaiter);
+            }
+            if (auto* awaiter = awaiters.get_lower(lastPublished - quater); awaiter != nullptr) {
+                barrier.add_awaiter(awaiter);
+            }
+        }
+    };
+
+    auto producer = [&]()
+    {
+        for (std::size_t i = 0; i <= std::numeric_limits<T>::max(); i++) {
+            barrier.publish(i);
+        }
+        producerDone.store(true, std::memory_order_release);
+        // Resume remaining awaiters
+        for (std::size_t i = 0; i <= std::numeric_limits<T>::max(); i++) {
+            barrier.publish(i);
+        }
+    };
+
+    thread_pool tp{2};
+    any_io_executor executorA = tp.get_executor();
+    any_io_executor executorB = tp.get_executor();
+    post(executorA, consumer);
+    post(executorB, producer);
+    tp.join();
+}
+
+} // namespace boost::asio::awaitable_ext::test
