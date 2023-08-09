55bde9b2fd95a9ec315b5760ab8fd0b46b647f1e
Dmitriy Vetutnev <d.vetutnev@gmail.com>
2023-08-10 01:37:38 +0300

    Update git ignore

---
+++ flake.nix
@@ -0,0 +1,11 @@
+{
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+
+  outputs = { self, nixpkgs }:
+    let
+      pkgs = import nixpkgs { system = "x86_64-linux" };
+    in
+    {
+      packages.x86_64-linux.default = pkgs.callPackage ./default.nix {};
+    };
+}
